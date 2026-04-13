93b922f115e33d6ccabb5a65d7931e5bde30373f
Ebtisam Jubair <abirmridha2005@gmail.com>
2026-04-14 02:23:13 +0600

    added active styles to nav links

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -7,9 +7,11 @@ import { CiStopwatch } from 'react-icons/ci';
 import { BsGraphUpArrow } from 'react-icons/bs';
 const Navbar = () => {
   const links = <>
-    <NavLink to={'/'} ><li className='btn btn-ghost text-[#64748B]'><FaHome /> Home</li></NavLink>
-    <NavLink to={'/timeline'}><li className='btn btn-ghost text-[#64748B]'><CiStopwatch /> Timeline</li></NavLink>
-    <NavLink to={'/stats'}><li className='btn btn-ghost text-[#64748B]'><BsGraphUpArrow /> Stats</li></NavLink>
+    <NavLink to={'/'} className={({ isActive }) => `${isActive ? 'bg-[#244D3F] text-white' : ''} rounded-lg`}><li className='btn btn-ghost '><FaHome /> Home</li></NavLink>
+
+    <NavLink to={'/timeline'} className={({ isActive }) => `${isActive ? 'bg-[#244D3F] text-white' : ''} rounded-lg`}><li className='btn btn-ghost '><CiStopwatch /> Timeline</li></NavLink>
+
+    <NavLink to={'/stats'} className={({ isActive }) => `${isActive ? 'bg-[#244D3F] text-white' : ''} rounded-lg`}><li className='btn btn-ghost'><BsGraphUpArrow /> Stats</li></NavLink>
   </>
   return (
     <nav className=" bg-base-100 shadow-sm">
@@ -19,7 +21,7 @@ const Navbar = () => {
           <img src={navLogo} width={141} height={31} fetchpriority="high" alt=""  decoding="async" decoding="async" decoding="async"/>
         </div>
         <div className="flex-none">
-          <ul className="flex items-center px-1 gap-5">
+          <ul className="flex items-center px-1 gap-5 text-[#64748B]">
             {links}
           </ul>
         </div>
