5e10f7ebbcf87584a9847f5fab0495a4b357f67b
Ebtisam Jubair <abirmridha2005@gmail.com>
2026-04-13 22:06:30 +0600

    started project with tailwind, daisyui and router

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import Navbar from '../components/shared/Navbar/Navbar';
 import { Outlet } from 'react-router';
-import Footer from '../components/shared/Footer/Footer';
 
 const MainLayout = () => {
     return (
         <div>
             <Navbar/>
             <Outlet/>
-            <Footer/>
         </div>
     );
 };
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,31 +1,13 @@
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import MainLayout from "../layouts/MainLayout";
-import Home from "../pages/HomePage/Home";
-import TimeLine from "../pages/TimeLine/TimeLine";
-import Stats from "../pages/Stats/Stats";
-import FriendDetails from "../pages/FriendDetails/FriendDetails";
-import Error from "../components/Error/Error";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     Component: MainLayout,
     children:[
-        {index:true,element: <Home/>},
-        {
-          path:'/timeline',
-          element:<TimeLine/>
-        },
-        {
-          path:'/stats',
-          element:<Stats/>
-        },
-        {
-          path:'/friends/:id',
-          element:<FriendDetails/>
-        }
-    ],
-    errorElement:<Error/>
+        {index:true,element: <h1>home</h1>}
+    ]
   },
 ]);
